e0c5216b82f6f5f7553f0fb5e292af5a4be00550
Hendi Santika <hendisantika@gmail.com>
2018-11-06 06:53:49 +0700

    #17 - Remove unnecessary folder

--- src/main/scala/basic/ReadWrite.scala
+++ src/main/scala/basic/ReadWrite.scala
@@ -19,29 +19,22 @@ object ReadWrite {
     val writer = new PrintWriter(new File("/home/hendisantika/Documents/mainspring/data_kerja/crawler/sql/mobil123_output_" + tgl + ".csv"))
     var i = 36050
     for (line <- Source.fromFile(new File(filename), "ISO-8859-1").getLines()) {
-      writer.write(s""""${i + 1}"""" + "," + line+"\n")
-      println(s""""${i + 1}"""" + "," + line)
+      //      writer.write(s""""${i + 1}"""" + "," + line+"\n")
+      //      writer.write(s"${i + 1}, $line\n")
+      val cols = line.split(',').map(_.trim)
+      writer.write(s"${i + 1},${cols(0)},${cols(1)},${cols(2)},${cols(3)},${cols(4)},${cols(5)},${cols(6)},${cols(7)},${cols(8)}," +
+        s"${cols(9)},${cols(10)},${cols(11)},${cols(12)},${cols(13)},${cols(14)},${cols(15)},${cols(16)},${cols(17)}${cols(18)}," +
+        s"${cols(19)},${cols(20)},${cols(21)},${cols(22)},${cols(23)},${cols(24)},${cols(25)},0,${cols(27)}\n")
+
+      println(s"${i + 1}, ${cols(0)},${cols(1)},${cols(2)},${cols(3)},${cols(4)},${cols(5)},${cols(6)},${cols(7)},${cols(8)}," +
+        s"${cols(9)},${cols(10)},${cols(11)},${cols(12)},${cols(13)},${cols(14)},${cols(15)},${cols(16)},${cols(17)}${cols(18)}," +
+        s"${cols(19)},${cols(20)},${cols(21)},${cols(22)},${cols(23)},${cols(24)},${cols(25)},0,${cols(27)}\n")
       i += 1
     }
     writer.close()
+    println("Bereszzzs ....!")
 
   }
 
-  def write2File(data: String) {
 
-    val writer = new PrintWriter(new File("/home/hendisantika/Documents/testing/ThresholdOutput.txt"))
-
-    writer.write(data)
-
-    writer.close()
-
-  }
-
-  def readFile(filename: String): Unit = {
-    var i = 0
-    for (line <- Source.fromFile(new File(filename), "ISO-8859-1").getLines()) {
-      println(s""""$i ${line}"""" + ", ")
-      i += 1
-    }
-  }
 }
